857639cbd70ee040e9c5f5af16f72485db6c3f09
Britani Prescott <britanierin@me.com>
2019-11-01 22:53:16 -0500

    Initial Commit - Set up Leet Code Problem: Jewels and Stones

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCodeJewelsAndStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countJewels = NumJewelsInStones("aA", "aAAbbbb");
+            Console.WriteLine("The number of stones that are jewels is: " + countJewels);
+        }
+
+        public static int NumJewelsInStones(string J, string S)
+        {
+            int count = 0;
+            foreach(var chS in S)
+            {
+                foreach(var chJ in J)
+                {
+                    if (chS == chJ)
+                    {
+                        ++count;
+                        break;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
