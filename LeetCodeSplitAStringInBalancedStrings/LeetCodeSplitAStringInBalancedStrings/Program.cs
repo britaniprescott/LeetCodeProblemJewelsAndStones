82b9b839a7bca16a5fe7980d03686a9cd05a6d9f
Britani Prescott <britanierin@me.com>
2019-11-02 00:22:28 -0500

    initial commit for string in balanced strings problem"

---
+++ LeetCodeSplitAStringInBalancedStrings/LeetCodeSplitAStringInBalancedStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LeetCodeSplitAStringInBalancedStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public static int BalancedStringSplit(string s)
+    {
+        
+    }
+}
