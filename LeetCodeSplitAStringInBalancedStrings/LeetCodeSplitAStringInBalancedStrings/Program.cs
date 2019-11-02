1d1bfed07c388388fc61484968f399dadd9f2aa0
Britani Prescott <britanierin@me.com>
2019-11-02 01:40:39 -0500

    Created method and test for maximizing number of balanced substrings in a balanced string

--- LeetCodeSplitAStringInBalancedStrings/LeetCodeSplitAStringInBalancedStrings/Program.cs
+++ LeetCodeSplitAStringInBalancedStrings/LeetCodeSplitAStringInBalancedStrings/Program.cs
@@ -6,12 +6,37 @@ namespace LeetCodeSplitAStringInBalancedStrings
     {
         static void Main(string[] args)
         {
+            int count = BalancedStringSplit("RLLLLRRRLR");
+            Console.WriteLine("The number of \"balanced\" strings within the string is: " + count);
+        }
+        public static int BalancedStringSplit(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
 
+            var count = 0;
+            var r = 0;
+            var l = 0;
+            foreach(var ch in s)
+            {
+                if (ch == 'R')
+                {
+                    ++r;
+                }
+                else if (ch == 'L')
+                {
+                    ++l;
+                }
+
+                if (r == l)
+                {
+                    ++count;
+                }
+            }
+            return count;
         }
     }
 
-    public static int BalancedStringSplit(string s)
-    {
-        
-    }
 }
